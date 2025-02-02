9b31f9e3e7f316a40d6fe43e7a4b795fcc4e64d7
Yury Moskov <aerobus.778@gmail.com>
2025-02-02 15:11:43 +0300

    atlant file explorer

--- source/gem.d
+++ source/gem.d
@@ -1,6 +1,7 @@
 module atlant.gem;
 
 import std.process: execute;
+import std.file: DirEntry;
 import std.file: read;
 
 struct GemData
@@ -28,10 +29,16 @@ class CutGem
 		import std.stdio;
 		writeln("\tGem #", hash, ',', path, ',', uniqueHash, ',', payload.mime);
 	}
+
+	abstract public void touch(); // Gentle touch
+	abstract public void load();
 }
 
 class FileGem : CutGem
 {
+	private string fsPath;
+	public DirEntry entry;
+
 	private this(string reqPath)
 	{
 		uniqueHash = true;
@@ -39,25 +46,57 @@ class FileGem : CutGem
 		payload = new GemData();
 	}
 
-	public this(string reqPath, string fsPath, bool isDir)
+	public this(string reqPath, string fsPath, DirEntry entry)
 	{
-        import std.string: strip;
 		this(reqPath);
+		this.fsPath = fsPath;
+		this.entry = entry;
+	}
 
+	override
+	public void load()
+	{
+		import std.string: strip;
 		auto result = execute(["file", "-ib", fsPath]);
 		payload.mime = strip(result.output);
 		payload.data = cast(immutable(void)[]) read(fsPath);
 	}
+
+	override
+	public void touch()
+	{
+		if (payload.mime is null)
+		{
+			import std.string: strip;
+			auto result = execute(["file", "-ib", fsPath]);
+			payload.mime = strip(result.output);
+		}
+	}
 }
+
+import std.container.slist;
 
 class DirGem : FileGem
 {
-	public this(string reqPath, string fsPath, bool isDir)
+	// Filled only if scanner track directories enabled
+	public SList!DirGem subdirectories;
+	public SList!FileGem files;
+	private bool track;
+
+	public this(string reqPath, string fsPath, bool track, DirEntry entry)
+	{
+		super(reqPath);
+		this,fsPath = fsPath;
+		payload.dirty = true;
+		this.track = track;
+		this.entry = entry;
+	}
+
+	override
+	public void load()
 	{
 		import std.file: exists;
         import std.string: strip;
-		super(reqPath);
-
 		// index file
 		// TODO: config indices files
 		if (exists(fsPath ~ "/index.html"))
@@ -66,19 +105,84 @@ class DirGem : FileGem
 			auto result = execute(["file", "-ib", filename]);
 			payload.mime = strip(result.output);
 			payload.data = cast(immutable(void)[]) read(filename);
+			payload.dirty = false;
 		}
 		else
 		{
 			/*
 				Track directory contents by default
 			*/
-			payload.dirty = true;
-			track = true;
+			if (track)
+			{
+				import std.range: chain;
+				import std.string: representation;
+				import std.array: appender;
+				auto contents = appender!string;
+				contents.put("\x3c!doctype html>\x3chtml>\x3chead>\x3ctitle>Atlant ");
+				contents.put(path);
+				contents.put("\x3c/title>\x3cstyle>table { width: 100%; }\x3c/style>\x3c/head>\x3cbody>\x3ch2>Atlant File Explorer ");
+				contents.put(path);
+				contents.put("\x3c/h2>\x3ctable>\x3ctr>\x3ctd>\x3c/td>\x3ctd>Name\x3c/td>\x3ctd>Content-Type\x3c/td>\x3ctd>Request Path Hash\x3c/td>\x3ctd>Content-Length\x3c/td>\x3c/tr>");
+				if (path != "/")
+				{
+					contents.put("\x3ctr>\x3ctd>d\x3c/td>\x3ctd>\x3ca href=\"..\">..\x3c/a>\x3c/td>\x3ctd>\x3c/td>\x3ctd>\x3c/td>\x3ctd>\x3c/td>\x3c/tr>");
+				}
+
+				//auto middle = appender!FileGem;
+				//middle.put(files);
+				auto x = chain(subdirectories[], files[]);
+				//SList!FileGem x = (cast(SList!FileGem)subdirectories) ~ ;
+
+				foreach (gem; x)
+				{
+					import std.path: baseName;
+					import std.conv;
+
+					auto flags = "-";
+					auto reqHtml = gem.path;
+					auto entry = gem.entry;
+					if (entry.isDir())
+					{
+						flags = "d";
+						reqHtml ~= "/";
+					}
+					if (entry.isSymlink())
+					{
+						flags = "l";
+					}
+					contents.put("\x3ctr>\x3ctd>");
+					contents.put(flags);
+					contents.put("\x3c/td>\x3ctd>\x3ca href=\"");
+					contents.put(reqHtml);
+					contents.put("\">");
+					contents.put(baseName(entry));
+					contents.put("\x3c/a>\x3c/td>\x3ctd>");
+					contents.put(gem.payload.mime);
+					contents.put("\x3c/td>\x3ctd>0x");
+					contents.put(to!string(gem.hash, 16));
+					contents.put("\x3c/td>\x3ctd>");
+					contents.put(to!string(entry.size));
+					contents.put("\x3c/td>\x3c/tr>");
+				}
+
+				contents.put("\x3c/table>\x3c/body>\x3c/html>");
+
+				this.payload.dirty = false;
+				this.payload.data = contents.data().representation();
+				this.payload.mime = "text/html; charset=utf-8";
+			}
+			else
+			{
+				payload.dirty = true;
+			}
 		}
 	}
-	public bool track;
-}
 
+	override
+	public void touch()
+	{
+	}
+}
 
 class ProxyGem : CutGem
 {
@@ -87,5 +191,17 @@ class ProxyGem : CutGem
 		uniqueHash = true;
 		super(reqPath);
 		payload = original.payload;
+	}
+
+	override
+	public void load()
+	{
+
+	}
+
+	override
+	public void touch()
+	{
+
 	}
 }
--- source/scanner.d
+++ source/scanner.d
@@ -34,10 +34,7 @@ class Scanner
 
 	protected DirGem scanDirectory(string path, string reqPath)
 	{
-		import std.string: representation;
-		import std.array: appender;
-		auto contents = appender!string;
-		auto directoryGem = new DirGem(reqPath ~ "/", path, true);
+		auto directoryGem = new DirGem(reqPath ~ "/", path, this.trackDirectories, DirEntry(path));
 		auto proxyGem = new ProxyGem(directoryGem, reqPath);
 
 		gems.insert(directoryGem);
@@ -47,19 +44,6 @@ class Scanner
 
 		reqPath ~= "/";
 
-		if (this.trackDirectories && directoryGem.track)
-		{
-			contents.put("<!doctype html><html><head><title>Atlant ");
-			contents.put(reqPath);
-			contents.put("</title><style>table { width: 100%; }</style></head><body><h2>Atlant File Explorer ");
-			contents.put(reqPath);
-			contents.put("</h2><table><tr><td></td><td>Name</td><td>Content-Type</td><td>Request Path Hash</td><td>Content-Length</td></tr>");
-			if (reqPath != "/")
-			{
-				contents.put("<tr><td>d</td><td><a href=\"..\">..</a></td><td></td><td></td><td></td></tr>");
-			}
-		}
-
 		import std.path: baseName;
 		foreach(DirEntry entry; dirEntries(path, SpanMode.shallow))
 		{
@@ -67,56 +51,26 @@ class Scanner
 			string fullPath = entry.name;
 			string req = reqPath ~ name;
 
-			FileGem gem;
 			if (entry.isDir())
 			{
-				gem = scanDirectory(fullPath, req);
+				DirGem child = scanDirectory(fullPath, req);
+				if (this.trackDirectories)
+				{
+					directoryGem.subdirectories.insert(child);
+				}
 			}
 			else
 			{
-				gem = new FileGem(req, fullPath, false);
+				FileGem gem = new FileGem(req, fullPath, entry);
 				gems.insert(gem);
 				this.counter++;
-			}
-
-			if (this.trackDirectories && directoryGem.track)
-			{
-				import std.conv;
-				auto flags = "-";
-				auto reqHtml = req;
-				if (entry.isDir())
+				if (this.trackDirectories)
 				{
-					flags = "d";
-					reqHtml ~= "/";
+					directoryGem.files.insert(gem);
 				}
-				if (entry.isSymlink())
-				{
-					flags = "l";
-				}
-				contents.put("<tr><td>");
-				contents.put(flags);
-				contents.put("</td><td><a href=\"");
-				contents.put(reqHtml);
-				contents.put("\">");
-				contents.put(name);
-				contents.put("</a></td><td>");
-				contents.put(gem.payload.mime);
-				contents.put("</td><td>0x");
-				contents.put(to!string(gem.hash, 16));
-				contents.put("</td><td>");
-				contents.put(to!string(entry.size));
-				contents.put("</td></tr>");
 			}
 		}
 
-		if (this.trackDirectories && directoryGem.track)
-		{
-			contents.put("</table></body></html>");
-
-			directoryGem.payload.dirty = false;
-			directoryGem.payload.data = contents.data().representation();
-			directoryGem.payload.mime = "text/html; charset=utf8";
-		}
 		return directoryGem;
 	}
 
@@ -129,6 +83,10 @@ class Scanner
 
 	public HashTable build()
 	{
-        return new HashTable(this.getCounter(), this.getGems());
+		foreach (gem; this.getGems())
+		{
+			gem.load();
+		}
+		return new HashTable(this.getCounter(), this.getGems());
 	}
 }
