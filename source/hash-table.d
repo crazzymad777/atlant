c3a788502baffe1a62a79dec64b177e7301e2e01
Yuriy Moskoff <aerobus.778@gmail.com>
2025-03-10 08:39:44 +0300

    better-c: bring back own HashTable implementation

--- source/hash-table.d
+++ source/hash-table.d
@@ -3,21 +3,85 @@ module atlant.hash_table;
 import std.container.slist;
 import atlant.gem;
 
+struct Bucket
+{
+	public this(long capacity)
+	{
+		gems = new CutGem*[capacity];
+	}
+	private long length;
+	private CutGem*[] gems;
+
+	public void put(CutGem* newGem)
+	{
+		for (long i = 0; i < length; i++)
+		{
+			if (gems[i].hash == newGem.hash)
+			{
+				gems[i].uniqueHash = false;
+				newGem.uniqueHash = false;
+			}
+		}
+
+		gems[length] = newGem;
+		length++;
+	}
+
+	public CutGem* find(string path, long hash)
+	{
+		int i = 0;
+		for (; i < length; i++)
+		{
+			if (gems[i].hash == hash)
+			{
+				if (gems[i].uniqueHash)
+				{
+					break;
+				}
+				else if (gems[i].path == path)
+				{
+					break;
+				}
+			}
+		}
+		return i != length ? gems[i] : null;
+	}
+}
+
 struct HashTable
 {
-	CutGem*[string] hashMap;
+	private Bucket*[] buckets;
+	public long reducer;
 	public this(long counter, SList!(CutGem*) gems)
 	{
+		reducer = counter;
+		buckets = new Bucket*[reducer];
+		int[] counts = new int[reducer];
 		foreach (x; gems)
 		{
-			hashMap[x.path] = x;
+			long index = x.hash % reducer;
+			counts[index]++;
 		}
+
+		foreach (x; gems)
+		{
+			long index = x.hash % reducer;
+			Bucket* bucket = buckets[index];
+			if (bucket is null)
+			{
+				bucket = new Bucket(counts[index]);
+				buckets[index] = bucket;
+			}
+
+			bucket.put(x);
+		}
+
 		rehash();
 	}
 
 	public void rehash()
 	{
-		hashMap.rehash;
+		// hashMap.rehash;
 	}
 
 	public CutGem* search(string path)
@@ -27,11 +91,13 @@ struct HashTable
 			path ~= '/';
 		}
 
-		auto x = path in hashMap;
-		if (x is null)
+		long hash = object.hashOf(path);
+		long index = hash%reducer;
+		Bucket* bucket = buckets[index];
+		if (bucket is null)
 		{
 			return null;
 		}
-		return *x;
+		return bucket.find(path, hash);
 	}
 }
