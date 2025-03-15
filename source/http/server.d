c5c2d9fc261cf3d5f58174a6eef512619d800b2a
Yuriy Moskoff <aerobus.778@gmail.com>
2025-03-15 22:45:42 +0300

    better-c: introduce http.server

---
+++ source/http/server.d
@@ -0,0 +1,47 @@
+module atlant.http.server;
+
+struct ServerInstance
+{
+    private int sockfd = -1;
+
+    void listen()
+    {
+        import core.sys.posix.netinet.in_;
+        import core.sys.posix.sys.socket;
+        import core.sys.posix.unistd;
+        sockaddr_in servaddr;
+        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd == -1)
+        {
+            return;
+        }
+
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        servaddr.sin_port = htons(8080);
+
+        if (bind(sockfd, cast(sockaddr*) &servaddr, servaddr.sizeof) != 0)
+        {
+            return;
+        }
+    }
+
+    ~this()
+    {
+        import core.sys.posix.unistd;
+        if (sockfd > 0)
+        {
+            close(sockfd);
+        }
+    }
+}
+
+struct Server
+{
+    ServerInstance instance;
+
+    void listen()
+    {
+        instance.listen();
+    }
+}
