2af2d85e04dadda77e0552eb533c949677598986
Yuriy Moskoff <aerobus.778@gmail.com>
2025-05-10 17:12:25 +0300

    feat-betterc: update Cache package

--- source/cache/bucket.d
+++ source/cache/bucket.d
@@ -1,13 +1,52 @@
 module atlant.cache.bucket;
 
 import atlant.utils.string;
+import atlant.utils.array;
 import atlant.cache.gem;
 
 // Bucket contains gems with same perfect hash
 struct Bucket
 {
-    Gem* find(String* entry)
+	public this(long capacity)
 	{
-        return null;
+		gems = Array!(Gem*)(capacity);
+	}
+	private long length;
+	private Array!(Gem*) gems;
+
+	public void put(Gem* newGem)
+	{
+		for (long i = 0; i < length; i++)
+		{
+			if (gems.at(i).hash == newGem.hash)
+			{
+				gems.at(i).uniqueHash = false;
+				newGem.uniqueHash = false;
+			}
+		}
+
+		gems.put(length, newGem);
+		length++;
+	}
+
+	Gem* find(String* entry)
+	{
+        int i = 0;
+		for (; i < length; i++)
+		{
+            Gem* gem = gems.at(i);
+			if (gem.hash == entry.hashOf())
+			{
+				if (gem.uniqueHash)
+				{
+					break;
+				}
+				else if (entry.equals(&gem.path))
+				{
+					break;
+				}
+			}
+		}
+		return i != length ? gems.at(i) : null;
 	}
 }
--- source/cache/gem.d
+++ source/cache/gem.d
@@ -1,6 +1,12 @@
 module atlant.cache.gem;
 
+import atlant.utils.string;
+
 // Gem stores mime type and content
 struct Gem
 {
+    String mime;
+    String path;
+    int hash;
+    bool uniqueHash;
 }
