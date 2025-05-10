929544ef174c9fdf6cd4ebcdbe7edc4c0620644e
Yuriy Moskoff <aerobus.778@gmail.com>
2025-05-10 19:13:07 +0300

    feat-betterc: Gem: of & drop & getMime

--- source/cache/gem.d
+++ source/cache/gem.d
@@ -6,8 +6,29 @@ import atlant.utils.string;
 // Gem stores mime type and content
 struct Gem
 {
+    static Gem* of(TreeNode* node)
+    {
+        import atlant.filesystem.utils;
+        import core.stdc.stdlib;
+        Gem* gem = cast(Gem*) malloc(Gem.sizeof);
+        assert(gem !is null);
+        gem.node = node;
+        gem.uniqueHash = true;
+        gem.hash = node.filename.hashOf();
+
+        getMime(&gem.mime, node.filename.data);
+        return gem;
+    }
+
     TreeNode* node;
     String mime;
     int hash;
     bool uniqueHash; // unique hash in the Bucket
+
+    void drop()
+    {
+        import core.stdc.stdlib;
+        mime.drop();
+        free(&this);
+    }
 }
