e9b672e1a215ff3a9e3044cf2e2d4fd0adf75e69
Yuriy Moskoff <aerobus.778@gmail.com>
2025-05-10 16:14:08 +0300

    feat-betterc: add findBucketIndex & getBucketByIndex

--- source/cache/hash_table.d
+++ source/cache/hash_table.d
@@ -1,16 +1,29 @@
 module atlant.cache.hash_table;
 
-// Hash-table -> bucket* -> gem[m]
+import atlant.filesystem.tree;
 
-uint touch(uint number, uint N)
-{
-    int rem = number%N;
-    return rem;
-}
+// Hash-table -> bucket* -> gem[m]
 
 struct HashTable
 {
+    private ulong count;
+    this(TreeNode root)
+    {
+        count = root.childsNumber;
+    }
+
     import atlant.cache.bucket;
     import atlant.utils.array;
 	private Array!(Bucket*) buckets;
+
+	uint findBucketIndex(uint number)
+	{
+        uint rem = number%count;
+        return rem;
+	}
+
+	Bucket* getBucketByIndex(uint index)
+	{
+        return buckets.at(index);
+	}
 }
