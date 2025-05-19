bfcce1bede1169af899551750c23e28fa3303a30
Yuriy Moskoff <aerobus.778@gmail.com>
2025-05-19 20:52:46 +0300

    IPv4 mapped address translation

--- source/http/server.d
+++ source/http/server.d
@@ -17,12 +17,54 @@ extern (C) void termination_handler(int signum) nothrow @nogc
     doWork = false;
 }
 
+import core.sys.posix.netinet.in_;
+void normalize(bool anyaddr, int family, sockaddr_in6* addr)
+{
+    // import core.sys.posix.netinet.in_;
+    import core.sys.posix.sys.socket;
+    if (anyaddr)
+    {
+        if (family == AF_INET6)
+        {
+            if (addr.sin6_addr.s6_addr[0x0a] == 0xff &&
+                addr.sin6_addr.s6_addr[0x0b] == 0xff)
+            {
+                int hits = 0;
+                for (; hits  < 0xa; hits ++)
+                {
+                    if (addr.sin6_addr.s6_addr[hits ] != 0)
+                    {
+                        break;
+                    }
+                }
+
+                if (hits == 0xa)
+                {
+                    // That's IPv4 address mapped to IPv6
+                    // Translation
+                    addr.sin6_family = AF_INET;
+                    addr.sin6_addr.s6_addr[0x00] = addr.sin6_addr.s6_addr[0x0c];
+                    addr.sin6_addr.s6_addr[0x01] = addr.sin6_addr.s6_addr[0x0d];
+                    addr.sin6_addr.s6_addr[0x02] = addr.sin6_addr.s6_addr[0x0e];
+                    addr.sin6_addr.s6_addr[0x03] = addr.sin6_addr.s6_addr[0x0f];
+
+                    for (int i = 0x0a; i <= 0x0f; i++)
+                    {
+                        addr.sin6_addr.s6_addr[i] = 0x0;
+                    }
+                }
+            }
+        }
+    }
+}
+
 struct ServerInstance
 {
     private int sockfd = -1;
     int family;
     char* addr;
     int port;
+    bool anyaddr;
 
     int try6()
     {
@@ -56,6 +98,7 @@ struct ServerInstance
         {
             servaddr.sin6_addr = in6addr_any;
             success = true;
+            anyaddr = true;
         }
 
         if (!success)
@@ -140,6 +183,7 @@ struct ServerInstance
         {
             servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
             success = true;
+            anyaddr = true;
         }
         else
         {
@@ -217,6 +261,7 @@ struct ServerInstance
             {
                 import atlant.http.session;
                 Session session = Session(conn);
+                normalize(anyaddr, family, &clientaddr);
                 pid = session.spawn(family, clientaddr);
                 if (pid == 0)
                 {
